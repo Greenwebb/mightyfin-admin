ebdda18818c05d134370b1b79bdb109c9b44aa72
br3-mah <nyeleti.bremah@gmail.com>
2024-02-19 10:50:49 +0200

    okay

--- database/migrations/2023_03_22_094157_remove_email_unique_constraint_from_users_table.php
+++ database/migrations/2023_03_22_094157_remove_email_unique_constraint_from_users_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropUnique(['email']);
-        });
+        // Schema::table('users', function (Blueprint $table) {
+        //     $table->dropUnique(['email']);
+        // });
     }
 
     /**
