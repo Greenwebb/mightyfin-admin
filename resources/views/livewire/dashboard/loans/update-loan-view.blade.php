2ca52b74c0088176f677ba6fb36922d611190d1f
Zindi <br3-mah>
2024-04-11 12:16:30 +0200

    admin 500 error

--- resources/views/livewire/dashboard/loans/update-loan-view.blade.php
+++ resources/views/livewire/dashboard/loans/update-loan-view.blade.php
@@ -1,6 +1,6 @@
 
 <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
-    <a href="{{ route('item-settings', ['confg' => 'loan','settings' => 'loan-types']) }}" class="flex py-4 px-9">
+    <a href="{{ route('loans') }}" class="flex py-4 px-9">
         <span>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-90deg-left" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M1.146 4.854a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H12.5A2.5 2.5 0 0 1 15 6.5v8a.5.5 0 0 1-1 0v-8A1.5 1.5 0 0 0 12.5 5H2.707l3.147 3.146a.5.5 0 1 1-.708.708z"/>
@@ -29,7 +29,7 @@
                         <h3 class="fw-bold text-info m-0">Edit Loan Information:</h3>
                     </div>
                 </div>
-                
+
                 <div id="kt_account_settings_profile_details" class="collapse show">
                     <div id="kt_account_profile_details_form" class="form">
                         <div class="card-body border-top p-9">
@@ -44,12 +44,12 @@
                                         @endforelse
                                     </select>
                                 </div>
-                            </div> 
+                            </div>
                             <div class="row mb-6">
                                 <label class="col-lg-4 col-form-label required fw-bold fs-6">Borrower</label>
                                 <div class="col-lg-8 fv-row">
-                                    <input disabled type="text" name="" value="{{ $user['fname'].' '.$user['lname']}}" class="form-control form-control-lg form-control-solid">                  
-                                    <input type="hidden" value="{{ $user['id'] }}" name="borrower_id" class="form-control">                           
+                                    <input disabled type="text" name="" value="{{ $user['fname'].' '.$user['lname']}}" class="form-control form-control-lg form-control-solid">
+                                    <input type="hidden" value="{{ $user['id'] }}" name="borrower_id" class="form-control">
                                 </div>
                             </div>
                             {{-- <div class="row mb-6">
@@ -95,7 +95,7 @@
                         <h3 class="fw-bold text-info m-0">Next of Kin:</h3>
                     </div>
                 </div>
-                
+
                 <div id="kt_account_settings_profile_details" class="collapse show">
                     <div id="kt_account_profile_details_form" class="form">
                         <div class="card-body border-top p-9">
@@ -133,7 +133,7 @@
                     </div>
                 </div>
             </div> --}}
-            
+
 
             {{-- <div class="card mb-5 mb-xl-10">
                 <div class="card-header border-0 cursor-pointer" role="button" data-bs-toggle="collapse" data-bs-target="#kt_account_profile_details" aria-expanded="true" aria-controls="kt_account_profile_details">
@@ -141,11 +141,11 @@
                         <h3 class="fw-bold text-info m-0">References Information:</h3>
                     </div>
                 </div>
-                
+
                 <div id="kt_account_settings_profile_details" class="collapse show">
                     <div id="kt_account_profile_details_form" class="form">
                         <div class="card-body border-top p-9">
-                            
+
                         <h4 class="text-gray py-3" style="color: darkgray">Human Resources</h4>
                             <div class="row mb-6">
                                 <label class="col-lg-4 col-form-label required fw-bold fs-6">First Name</label>
@@ -167,7 +167,7 @@
                             </div>
                         </div>
                         <div class="card-body border-top p-9">
-                            
+
                         <h4 class="text-gray py-3" style="color: darkgray">Supervisor </h4>
                             <div class="row mb-6">
                                 <label class="col-lg-4 col-form-label required fw-bold fs-6">First Name</label>
@@ -191,18 +191,18 @@
                     </div>
                 </div>
             </div> --}}
-            
+
             <div class="card mb-5 mb-xl-10">
                 <div class="card-header border-0 cursor-pointer" role="button" data-bs-toggle="collapse" data-bs-target="#kt_account_profile_details" aria-expanded="true" aria-controls="kt_account_profile_details">
                     <div class="card-title m-0">
                         <h3 class="fw-bold text-info m-0">Documents Upload:</h3>
                     </div>
                 </div>
-                
+
                 <div id="kt_account_settings_profile_details" class="collapse show">
                     <div id="kt_account_profile_details_form" class="form">
                         <div class="card-body border-top p-9">
-                            
+
                             <div class="row mb-6">
                                 <label class="col-lg-4 col-form-label required fw-bold fs-6">NRC</label>
                                 <div class="col-lg-8 fv-row">
@@ -242,12 +242,12 @@
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-floppy2" viewBox="0 0 16 16">
                                 <path d="M1.5 0h11.586a1.5 1.5 0 0 1 1.06.44l1.415 1.414A1.5 1.5 0 0 1 16 2.914V14.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 14.5v-13A1.5 1.5 0 0 1 1.5 0M1 1.5v13a.5.5 0 0 0 .5.5H2v-4.5A1.5 1.5 0 0 1 3.5 9h9a1.5 1.5 0 0 1 1.5 1.5V15h.5a.5.5 0 0 0 .5-.5V2.914a.5.5 0 0 0-.146-.353l-1.415-1.415A.5.5 0 0 0 13.086 1H13v3.5A1.5 1.5 0 0 1 11.5 6h-7A1.5 1.5 0 0 1 3 4.5V1H1.5a.5.5 0 0 0-.5.5m9.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5z"/>
                             </svg>
-                        </span>    
+                        </span>
                         Save
                     </button>
                 </div>
             </div>
-            
+
         </form>
     </div>
 </div>
